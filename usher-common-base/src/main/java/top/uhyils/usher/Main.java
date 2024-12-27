2d356a9c561c0a81839c6e865a1fa87ebdc9e47f
龙龙 <shangyilong@haier.com>
2024-12-27 09:59:15 +0800

    init

---
+++ usher-common-base/src/main/java/top/uhyils/usher/Main.java
@@ -0,0 +1,13 @@
+package top.uhyils.usher;
+
+
+/**
+ * @author uhyils <247452312@qq.com>
+ * @date 文件创建日期 2024年12月27日 08时40分
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
